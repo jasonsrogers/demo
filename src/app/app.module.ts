62ecb2b97a00d5a020a1b23dba8b4708a21bf59b
Jason Rogers <jason.s.rogers@gmail.com>
2023-02-06 15:15:08 +0100

    fix lib path name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CommonModule } from '@angular/common';
-import { SharedUiModule } from 'shared/ui';
+import { SharedUiModule } from '@demo-app/shared/ui';
 import { AiPageComponent } from './ai-page/ai-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
