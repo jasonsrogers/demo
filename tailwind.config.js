292f1593ce8798e892807284948b4cfb8f8971d8
Jason Rogers <jason.s.rogers@gmail.com>
2023-02-14 15:17:04 +0100

    fix of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {},
   plugins: [],
+  prefix: 'tw-',
 };
