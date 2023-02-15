e0891d06389abd9c87dc524a1750e53eecfc3a1d
Jason Rogers <jason.s.rogers@gmail.com>
2023-02-15 12:45:50 +0100

    clock tweak + app header

--- shared/ui/src/index.ts
+++ shared/ui/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/shared-ui.module';
 
 export * from './lib/menu/menu.component';
+
+export * from './lib/app-header/app-header.component';
--- shared/ui/src/lib/shared-ui.module.ts
+++ shared/ui/src/lib/shared-ui.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AnalogClockComponent } from './analog-clock/analog-clock.component';
 import { MenuComponent } from './menu/menu.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [MenuComponent, AnalogClockComponent],
-  exports: [MenuComponent, AnalogClockComponent],
+  declarations: [MenuComponent, AnalogClockComponent, AppHeaderComponent],
+  exports: [MenuComponent, AnalogClockComponent, AppHeaderComponent],
 })
 export class SharedUiModule {}
